5addd0d1fae4dbfe1f6f7b39dd5a1122463b7728
Yuki Yokotani <yokotani.yuki@gmail.com>
2021-11-28 23:12:39 +0900

    cusorをpointerに変更

--- src/features/table/Square.tsx
+++ src/features/table/Square.tsx
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme: Theme) =>
       fontSize: '2rem',
       width: '3rem',
       height: '3rem',
-      cursor: 'default',
+      cursor: 'pointer',
       '-webkit-touch-callout': 'none' /* iOS Safari */,
       '-webkit-user-select': 'none' /* Safari */,
       '-khtml-user-select': 'none' /* Konqueror HTML */,
